0dc8b0d0bd5d4d4910bf06901152075b8c01a55e
espoirndayishimiye4 <espoirndayishimiye4@gmail.com>
2022-07-12 17:06:31 -0700

    changes

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ app.use(express.json())
 
 const PORT = 3000
 
+//middleware to console time when any http method is requested
 app.use(time)
+
+//http://localhost:3000/staffs
 app.use('/staffs', require('./routes/staffs'))
+
+//http://localhost:3000/trainees
 app.use('/trainees',require('./routes/trainees'))
+
+//http://localhost:3000/trainers
 app.use('/trainers',require('./routes/trainers'))
 
 app.listen(PORT,()=>{console.log(`server is running on port ${PORT}`)})
